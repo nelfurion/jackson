87ff184e7f386abf318bf5cc4077c4074831c8bd
nelfurion <angelovradostin@gmail.com>
2017-03-28 09:13:18 +0300

    edited edit_bllip.sh

--- edit_bllip.sh
+++ edit_bllip.sh
@@ -1,1 +1,1 @@
-cp ./bin/bllip.py /home/$USER/anaconda3/envs/jackson_env/lib/python3.5/site-packages/nltk/parse/bllip.py
+cp ./bin/bllip.py /home/$USER/anaconda3/envs/$1/lib/python3.5/site-packages/nltk/parse/bllip.py
