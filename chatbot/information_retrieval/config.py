6e6d89488c3f9f92eba88cb2bf6d8614392faa89
nelfurion <angelovradostin@gmail.com>
2017-05-30 16:42:02 +0300

    updated endpoints

--- chatbot/information_retrieval/config.py
+++ chatbot/information_retrieval/config.py
@@ -1,4 +1,10 @@
 config = {
-    'summarization_worker_endpoint': 'http://127.0.0.1:8000/summarization/summarize/',
-    'summarization_job_endpoint': 'http://127.0.0.1:8000/summarization/summary/'
+    'summarization_worker_endpoint': 'https://mighty-lake-38379.herokuapp.com/summarization/summarize/',
+    'summarization_job_endpoint': 'https://mighty-lake-38379.herokuapp.com/summarization/summary/'
 }
+
+'''
+local
+'summarization_worker_endpoint': 'http://127.0.0.1:8000/summarization/summarize/',
+'summarization_job_endpoint': 'http://127.0.0.1:8000/summarization/summary/'
+'''
--- summarization_worker/summarization_worker/summarization/summarization/config.py
+++ summarization_worker/summarization_worker/summarization/summarization/config.py
@@ -5,5 +5,10 @@ config = {
         'https://mysterious-falls-92665.herokuapp.com/score/',
         'https://pure-depths-52550.herokuapp.com/score/',
         'https://agile-taiga-29459.herokuapp.com/score/',
+        'https://guarded-everglades-56581.herokuapp.com/score/',
+        'https://floating-scrubland-37232.herokuapp.com/score/',
+        'https://stormy-savannah-82712.herokuapp.com/score/',
+        'https://agile-reaches-65973.herokuapp.com/score/',
+        'https://secret-woodland-23863.herokuapp.com/score/'
     ]
 }
